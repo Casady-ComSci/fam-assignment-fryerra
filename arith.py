803a4c8381bc7ed8e1b9fa9ac0ae347389451457
Rachel Fryer <rachelfryer@casady.org>
2019-08-26 12:37:25 -0500

    Partially Completed Assignment
    yeet! i need to do assignment.md

--- arith.py
+++ arith.py
@@ -1,17 +1,29 @@
 # Write the python code to print 2*3
-
-
+```python3
+print(2*3)
+```
 # Write the python code to assign the integer 3 to x, the string "yes" to y, and
 # the integer 5 to z.
-
-
-
+```python3
+x=3
+y='yes'
+z=5
+```
 # Write the python code to print the type of y.
-
-
-
+```python3
+print(y)
+```
 # Write the python code to assign x+z to sum, and then print the sum.
-
-
+```python3
+x+z=sum
+print(sum)
+```
 # Store 2.5 in the variable x, 3.7 in the variable y, and their product in the
 # variable z. Now try to divide by zero and store in the variable a.
+```python3
+x=2.5
+y=3.7
+z=x*y
+z/0
+a=z/0
+```
--- written.md
+++ written.md
@@ -1,19 +1,25 @@
 # Some Questions to Answer
 
-1. Name:
+1. Name: Rachel
 
 2. Add the code to cube 2, store the result in a variable, and then print out that variable. (See instructions on how to add code to markdown.)
-
+```python
+x=5
+y=7
+print(x+y)
+```
 3. Sports you have played:
-    *
-    *
-    *
+    *volleyball
+    *soccer (MD)
+    *softball
 
 4. Favorite place to go to lunch.
+Freddie's
 
 5. My favorite foods are:
-    1.
-    2.
-    3.
+    1.ice cream
+    2.mango
+    3.bread
 
 6. I'm working to be better at...
+volleyball and studying for my classes
